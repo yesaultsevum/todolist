c83c6d7850aedf2b124e928021af92b0bad4baed
yesaultsevum <yesaultsevum@gmail.com>
2015-11-22 21:23:52 +0200

    jsLint

--- js/todolist.js
+++ js/todolist.js
@@ -1,147 +1,136 @@
+/*global
+alert, console, jQuery, window
+*/
+(function ($) {
 
-(function(){
+    'use strict';
 
-    $(window).on('beforeunload', function() {
+    $(window).on('beforeunload', function () {
         var todos = $('.localStor').html();
-        if(todos){
+        if (todos) {
             localStorage.setItem('todos', todos);
-        }else{
+        } else {
             localStorage.clear();
         }
-        
     });
 
-    $(document).on('click','a',function(e){
-         e.preventDefault(); 
+    $(document).on('click', 'a', function (e) {
+        e.preventDefault();
     });
 
-    if(localStorage.getItem('todos')){
-         $('.localStor').html(localStorage.getItem('todos'));
+    if (localStorage.getItem('todos')) {
+        $('.localStor').html(localStorage.getItem('todos'));
     }
 
-	var $input = $('.enter_task input');
-    var $button = $('.enter_task button');
-    var $button_clear = $('.clear');
-    var flag = false;
+    var $input = $('.enter_task input'),
+        $button = $('.enter_task button'),
+        $button_clear = $('.clear'),
+        flag = false;
+
     $input.focus();
-  
-    $button.on('click',function(e){
-        e.preventDefault(); 
-        if($input.val()!==""){
-             var listItem = $('.enter_task input').val();
-             AddTask(listItem);
-             $input.val("").focus();
-             //todos = $('.localStor').html();
-             //localStorage.setItem('todos', todos);
-        }else{
-             $input.focus();
+
+    function addTask(listItem) {
+        $('.new').append('<div class="wrap"></div>');
+        $('.new .wrap:empty').animate({height: '+=30px'});
+        setTimeout(function () {
+            $('.new .wrap:empty').append('<div class="create"><div class="icon" dissabled><a href=""><i class="fa fa-square-o"></i></a></div><div class="task"><li>' + listItem + '</li></div><div class="icon2"><a href=""><i class="fa fa-times"></i></a></div></div>');
+        }, 400);
+    }
+
+    function addMarked(listMarked) {
+        $('.finished').prepend('<div class="wrap"></div>');
+        $('.finished .wrap:empty').animate({height: '+=30px'});
+        setTimeout(function () {
+            $('.finished .wrap:empty').append('<div class="createMarked"><div class="iconMarked"><a href=""><i class="fa fa-check-square-o"></i></a></div><div class="task"><li>' + listMarked + '</li></div><div class="icon2"><a href=""><i class="fa fa-times"></i></a></div></div>');
+        }, 500);
+    }
+
+    function removeListItem(self) {
+        setTimeout(function () {
+            $(self.parent()).fadeOut(300);
+            $(self.parent().parent()).slideUp(1000);
+        }, 300);
+        setTimeout(function () {
+            $(self.parent().parent()).remove();
+        }, 2000);
+    }
+
+    $button.on('click', function (e) {
+        var listItem;
+        e.preventDefault();
+        if ($input.val() !== "") {
+            listItem = $('.enter_task input').val();
+            addTask(listItem);
+            $input.val("").focus();
+        } else {
+            $input.focus();
         }
     });
 
-    $button_clear.on('click',function(e){
-        e.preventDefault(); 
+    $button_clear.on('click', function (e) {
+        e.preventDefault();
         $('.localStor').remove();
     });
 
+    $(document).on('click', '.icon', function () {
+        var self,
+            listMarked;
 
-        
-        $(document).on('click','.icon',function(){
+        if (flag) {
+            return false;
+        }
+        flag = true;
 
-             if(flag) return false;
-             flag = true;
+        $('i', this).replaceWith('<i class="fa fa-check-square-o"></i>').attr({disabled: true});
+        self = $(this);
+        removeListItem(self);
+        $('.hr1').show(300);
 
-             $('i',this).replaceWith('<i class="fa fa-check-square-o"></i>').attr({disabled: true});
-             var self = $(this);
-             RemoveListItem(self);
-             $('.hr1').show(300);
+        listMarked = $(this).next().text();
+        setTimeout(function () {
+            addMarked(listMarked);
+        }, 300);
+        setTimeout(function () {
+            flag = false;
+        }, 500);
+    });
 
-             var listMarked = $(this).next().text();
-             setTimeout(function(){
-                 AddMarked(listMarked);
-             },300);
-             setTimeout(function(){
-                 flag = false;
-             },500);
+    $(document).on('click', '.iconMarked', function () {
+        var self, listItem;
+        if (flag) {
+            return false;
+        }
+        flag = true;
 
-             // setTimeout(function(){
-             //     todos = $('.localStor').html();
-             //     localStorage.setItem('todos', todos);
-             // },2000);
+        $('i', this).replaceWith('<i class="fa fa-square-o"></i>');
 
-             
-        });
-        
-        $(document).on('click','.iconMarked',function(){ 
-             if(flag) return false;
-             flag = true;
+        self = $(this);
+        removeListItem(self);
 
-             $('i',this).replaceWith('<i class="fa fa-square-o"></i>');
+        listItem = $(this).next().text();
+        setTimeout(function () {
+            addTask(listItem);
+        }, 300);
+        setTimeout(function () {
+            flag = false;
+        }, 500);
 
-             var self = $(this);
-             RemoveListItem(self);
+        setTimeout(function () {
+            if ($('.finished').children().length < 2) {
+                $('.hr1').hide(300);
+            }
+        }, 700);
+    });
 
-             var listItem = $(this).next().text();
-             setTimeout(function(){
-                 AddTask(listItem);
-             },300);
-             setTimeout(function(){
-                 flag = false;
-             },500);
+    $(document).on('click', '.icon2', function () {
+        var self = $(this);
+        $(self.parent()).fadeOut(300);
+        setTimeout(function () {
+            $(self.parent().parent()).slideUp(1000);
+        }, 300);
+        setTimeout(function () {
+            $(self.parent().parent()).remove();
+        }, 20);
+    });
 
-             setTimeout(function(){
-                 if($('.finished').children().length < 2){
-                     $('.hr1').hide(300);
-                 }
-             },700);  
-
-             // setTimeout(function(){
-             //     todos = $('.localStor').html();
-             //     localStorage.setItem('todos', todos);
-             // },2000);
-        });
-
-        $(document).on('click','.icon2',function(){
-             var self = $(this);
-             $(self.parent()).fadeOut(300);
-             setTimeout(function(){
-                 $(self.parent().parent()).slideUp(1000);
-             },300)
-             setTimeout(function(){
-                 $(self.parent().parent()).remove();
-             }, 2000);
-
-             // setTimeout(function(){
-             //     todos = $('.localStor').html();
-             //     localStorage.setItem('todos', todos);
-             // },2000);
-        });
-    
-    
-
-    function AddTask(listItem){
-         $('.new').append('<div class="wrap"></div>');
-         $('.new .wrap:empty').animate({height:'+=30px'});
-         setTimeout(function(){
-             $('.new .wrap:empty').append('<div class="create"><div class="icon" dissabled><a href=""><i class="fa fa-square-o"></i></a></div><div class="task"><li>'+ listItem +'</li></div><div class="icon2"><a href=""><i class="fa fa-times"></i></a></div></div>');
-         },400)
-    }
-
-    function AddMarked(listMarked){
-         $('.finished').prepend('<div class="wrap"></div>');
-         $('.finished .wrap:empty').animate({height:'+=30px'})
-         setTimeout(function(){
-             $('.finished .wrap:empty').append('<div class="createMarked"><div class="iconMarked"><a href=""><i class="fa fa-check-square-o"></i></a></div><div class="task"><li>'+ listMarked +'</li></div><div class="icon2"><a href=""><i class="fa fa-times"></i></a></div></div>');
-         },500)
-    }
-
-    function RemoveListItem(self){
-         setTimeout(function(){
-             $(self.parent()).fadeOut(300);
-             $(self.parent().parent()).slideUp(1000);
-         }, 300);
-         setTimeout(function(){
-             $(self.parent().parent()).remove();
-         }, 2000);
-    }
-	         
-})();
+}(jQuery));
